41833fca5cf2ae2c7380a64ffdeea84d1cc20ad3
Asif Jalil <62372647+asif-jalil@users.noreply.github.com>
2021-05-13 14:18:41 +0600

    done

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/*
+** Title: FileTree Application
+** Description: This application will made for API for file tree
+** Author: Asif
+** Date: 16.05.2021
+** 
+*/
+
+// dependencies
 const express = require("express");
 const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
@@ -5,7 +14,10 @@ const ObjectID = require("mongodb").ObjectID;
 require("dotenv").config();
 
 const uri = `mongodb+srv://foldertreeadmin:YHa77rr!EavEhNh@cluster0.4sath.mongodb.net/folderTree?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+});
 
 const app = express();
 
@@ -16,43 +28,52 @@ app.use(express.json());
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+   res.send("Hello World!");
 });
 
 client.connect((err) => {
-  const folderCollection = client.db("folderTree").collection("folders");
+   const folderCollection = client.db("folderTree").collection("folders");
 
-  app.get("/folders", (req, res) => {
-    folderCollection.find({}).toArray((err, doc) => {
-      res.send(doc);
-    });
-  });
+   // get all folder
+   app.get("/folders", (req, res) => {
+      folderCollection.find({}).toArray((err, doc) => {
+         res.send(doc);
+      });
+   });
 
-  app.patch("/addFolder/:id", (req, res) => {
-    const id = req.params.id;
-    const detail = req.body;
-    const folder = {
-      _id: ObjectID(),
-      ...detail,
-    };
-    console.log(id);
-    folderCollection.updateOne({ _id: ObjectID(id) }, { $push: { children: folder } }).then((result) => {
-      console.log(result.matchedCount > 0);
-    });
-  });
+   // API for folder add
+   app.patch("/addFolder/:id", (req, res) => {
+      const id = req.params.id;
+      const detail = req.body;
+      const folder = {
+         _id: ObjectID(),
+         ...detail,
+      };
+      console.log(id);
+      folderCollection
+         .findOneAndUpdate(
+            { _id: ObjectID(id) },
+            { $push: { children: folder } }
+         ) 
+         .then((result) => {
+            console.log(result.modifiedCount > 0);
+            res.send(result.ok > 0);
+         });
+   });
 
-  // app.patch("/deleteFolder/:id", (req, res) => {
-  //   const id = req.params.id;
+   // API for folder delete
+   app.patch("/deleteFolder/:id", (req, res) => {
+      const id = req.params.id;
 
-  //   folderCollection.updateOne(
-  //     {},
-  //     {$pull: {children: { _id: ObjectID(id)}}}
-  //   ).then((result) => {
-  //     console.log(result.deletedCount > 0);
-  //   });
-  // });
+      folderCollection
+         .findOneAndUpdate({}, { $pull: { children: { _id: ObjectID(id) } } })
+         .then((result) => {
+            console.log(result);
+            res.send(result.ok > 0);
+         });
+   });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+   console.log(`Example app listening at http://localhost:${port}`);
 });
